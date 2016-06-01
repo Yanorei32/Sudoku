a05dcba5206271847b5ff550a7298feea338bde6
Ray Yano <yanorei@hotmail.co.jp>
2016-06-01 11:06:12 +0900

    Make define file

--- main.c
+++ main.c
@@ -1,35 +1,37 @@
+/*\
+| | *GCC Options
+| |   -Wall -pedantic --std=C99
+\*/
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include "sudoku_def.h"
 
-#define N		3
-#define M		3
-#define TRUE	1
-#define FALSE	0
-typedef int bool;
 
-int board[N * M][M * N];
+int board[BOARD_N * BOARD_M][BOARD_M * BOARD_N];
 
 void board_horizontal_line_print(){
-	/* 区切りを表示 */
+	// 区切りを表示
 	printf("+-------+-------+-------+");
 
 	printf("\n");
 }
 
 bool is_print_horizontal_position(int pos){
-	/* 区切りを表示すべき場所の場合は1を、表示すべきでない場所の場合は0をreturn */
-	if( (pos + 1) % N == 0 )
-		return TRUE;
+	// 区切りを表示すべき場所の場合は1を、表示すべきでない場所の場合は0をreturn
+	if( (pos + 1) % BOARD_N == 0 )
+		return true;
 	else
-		return FALSE;
+		return false;
 }
 
 void board_print(){
 	int i,j;
 	board_horizontal_line_print();
-	for(i = 0;i < N * N;i++){
+	for(i = 0;i < BOARD_N * BOARD_N;i++){
 		printf("|");
-		for(j = 0;j < N * N;j++){
+		for(j = 0;j < BOARD_N * BOARD_N;j++){
 			if(board[i][j] != 0)
 				printf(" %d",board[i][j]);
 			else
@@ -44,25 +46,25 @@ void board_print(){
 }
 
 void board_read(const char *filename){
-	/* File Pointerを作成 */
+	// File Pointerを作成
 	FILE *fp;
-	/* 読みだしたデータの保管用 */
-	char buf[N + 1];
-	/* ループ用の変数 */
+	// 読みだしたデータの保管用
+	char buf[BOARD_N + 1];
+	// ループ用の変数
 	int i,j;
-	/* ファイルオープン */
+	// ファイルオープン
 	if( (fp = fopen(filename,"r")) == NULL ){
 		printf("ファイルのオープンに失敗しました。\n");
 		exit(EXIT_FAILURE);
 	}
 
-	/* %で始まる最初の行を読み飛ばす。 */
+	// %で始まる最初の行を読み飛ばす。
 	fscanf(fp,"%*[^\n]");
 
-	/* 1行ずつ読み込む */
+	// 1行ずつ読み込む
 	for(i = 0;(fscanf(fp,"%9s%*[^\n]",buf)) != EOF;i++){
-		/* 一文字ずつ読み込む */
-		for(j = 0;j < N * N;j++){
+		// 一文字ずつ読み込む
+		for(j = 0;j < BOARD_N * BOARD_N;j++){
 			if(buf[j] == '.'){
 				board[i][j] = 0;
 			}else if('0' <= buf[j] && buf[j] <= '9'){
---
+++ sudoku_def.h
@@ -0,0 +1,31 @@
+#ifndef SUDOKU_DEF
+	#define SUDOKU_DEF
+	#define BOARD_N	3
+	#define BOARD_M	3
+	
+	typedef struct Candidate_ {
+		int		Val;
+		bool	Possible;
+	} Candidate_t;
+
+	typedef struct Board_ {
+		int			Value;
+		bool		InitValue;
+		Candidate_t	Candidate[BOARD_N * BOARD_M];
+	} Board_t;
+
+	typedef struct ValNum_ {
+		int			Value;
+		int			Num;
+	} ValNum_t;
+
+	typedef struct Group_ {
+		Board_t		BoardTable[BOARD_N * BOARD_M];
+		ValNum_t	ValNumTable[BOARD_N * BOARD_M9];
+	} Group_t;
+
+	typedef struct SudokuTable_ {
+		Board_t		MainBoard[BOARD_N][BOARD_M];
+		Group_t		Groups[BOARD_M*BOARD_N+BOARD_N*BOARD_M+BOARD_N*BOARD_M];
+	} SudokuTable_t;
+#endif
